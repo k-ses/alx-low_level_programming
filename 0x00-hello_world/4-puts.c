1ebb7cea7cbd51014e41ecc97c37413778da7d27
k-ses <kelvinwachira02@gmail.com>
2022-06-02 23:30:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h> 
-int main(void)
  /**
   * main - return some integer at the end of the execution
   *
   * return - always 0 (successful execution of the program)
   */
+int main(void)
 {
 puts("Programming is like building a multilingual puzzle");
 return 0;
