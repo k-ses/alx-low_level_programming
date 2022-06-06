10f1aef6df7859a4189cecf62b0d9614afe43d40
k-ses <kelvinwachira02@gmail.com>
2022-06-06 07:18:35 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,24 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
+
+/* more headers goes there */
+/* betty style doc for function main goes there */
 /**
- *prints if random generated number is +tive, zero or -tive
- */
+*main - main function
+*
+* No Description:
+*
+* Return: 0
+*/
+
 int main(void)
+
 {
+
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-        else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is positive\n", n);
-	}
+/*your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n)
+}
+else
+{
+printf("%d is negative\n", n);
+}
 return (0);
+}
