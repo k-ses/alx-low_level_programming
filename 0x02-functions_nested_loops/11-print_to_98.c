3a3ccd3c9be2674a1879e58818adba8d03bf973b
k-ses <kelvinwachira02@gmail.com>
2022-06-08 16:58:32 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,55 +1,44 @@
 #include "main.h"
+
+#include <stdio.h>
 /**
-* print_to_98 - print_to_98 block
-* @n: int type integer
-* Description:print all natural numbers 
-* from zero to 98 followed by a new line
-*/
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: The number to start printing from
+ * Return: Always 0.
+ */
 void print_to_98(int n)
 {
-if (n > 98)
+if (n <= 98)
 {
-while(n >= 98)
+for (; n <= 98; n++)
 {
-if(n > 99)
+if (n == 98)
 {
-_putchar((n / 100) + '0');
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
+printf("%d", n);
+printf("\n");
+break;
 }
 else
 {
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
-}
-if (n != 98)
-{
-_putchar(',');
-_putchar(' ');
-}
-n--;
+printf("%d, ", n);
 }
 }
-else 
-{ 
-while (n <= 98)
-{
-if (n >9)
-{
-_putchar((n / 10) + '0');
-_putchar((n % 10) + '0');
 }
 else
 {
-_putchar((n % 10) + '0');
-}
-if (n != 98)
+for (; n >= 98; n--)
 {
-_putchar(',');
-_putchar(' ');
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
 }
-n++;
+else
+{
+printf("%d, ", n);
 }
 }
-_putchar('\n');
+}
+
 }
