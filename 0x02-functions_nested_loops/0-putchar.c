8c5d7b06e3101eb0d55ccca65cb86d88840ef5a9
k-ses <kelvinwachira02@gmail.com>
2022-06-08 01:56:39 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
-/**
-*main - main function
-*description prints _putchar
-*Return: 0
-*/
-int main(void)
-{
-char letters[8]= "_putchar"
-int k;
-for (k = 0; k < 8; k++)
-putchar(letters[k]);
-putchar("\n");
-return (0);
+#include <stdio.h>                                                                                                          
+/**                                                                                                                         
+*main - main function                                                                                                       
+*description prints _putchar                                                                                                
+*Return: 0                                                                                                                  
+*/                                                                                                                          
+int main(void)                                                                                                              
+{                                                                                                                           
+char h[8]= "_putchar";                                                                                                 
+int k;                                                                                                                      
+for (k = 0; k < 8; k++)                                                                                                     
+putchar(h[k]);                                                                                                        
+putchar('\n');                                                                                                              
+return (0);  
 }
