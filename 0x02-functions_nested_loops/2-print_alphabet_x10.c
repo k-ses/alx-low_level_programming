7ad7b465eb74a0ba642a05fc4c14f0da9b0415e5
k-ses <kelvinwachira02@gmail.com>
2022-06-08 14:06:54 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *print_alphabet_x10 - print_alphabet_x10 block
-*Description: print alphabet in lowercase followed by new 
+*Description: print alphabet in lowercase
 *followed by new line
 *Return: 0
 */
