21a803661ba3984f24eb7ca8a6b358f505cdea3d
k-ses <kelvinwachira02@gmail.com>
2022-06-08 03:27:16 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/**                                                                                                                         
-*main - main function                                                                                                       
-*description prints _putchar                                                                                                
-*Return: 0                                                                                                                  
-*/                                                                                                                          
-int main(void)                                                                                                              
-{                                                                                                                           
-char h[8]= "_putchar";                                                                                                 
-int k;                                                                                                                      
-for (k = 0; k < 8; k++)                                                                                                     
-putchar(h[k]);                                                                                                        
-putchar('\n');                                                                                                              
-return (0);  
+#include "main.h"
+/**
+*main - main function
+*description prints _putchar
+*Return: 0
+*/
+int main(void)
+{
+char j[8] = "_putchar"
+int k;
+for (k = 0; k < 8; k++)
+	_putchar(j[k]);
+_putchar('\n')
+	return (0);
 }
