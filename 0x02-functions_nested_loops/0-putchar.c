badc326f5ed4e9d1decdd28c1de6b6558d869eb0
k-ses <kelvinwachira02@gmail.com>
2022-06-08 12:24:36 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include "stdio.h"                                                                                                          
-/**                                                                                                                         
-* main - Entry point                                                                                                        
-* description - prints _putchar                                                                                             
-* Return: Always 0                                                                                                          
-*/                                                                                                                          
-int main(void)                                                                                                              
-{                                                                                                                           
+#include <stdio.h>
+/**
+* main - main function
+* description - prints _putchar
+* Return: 0
+*/
+int main(void)
+{
 _putchar('_');
-_putchar('p');                                                                                                           
-_putchar('u');                                                                                                              
-_putchar('t');                                                                                                              
-_putchar('c');                                                                                                              
-_putchar('h');                                                                                                             
-_putchar('a');                                                                                                             
-_putchar('r');                                                                                                              
-_putchar('\n');                                                                                                             return (0);                                                                                                         
-} 
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
