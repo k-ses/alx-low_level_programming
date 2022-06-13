26db0a5e28335d164fada86118c6dd34179c6b4c
k-ses <kelvinwachira02@gmail.com>
2022-06-13 06:37:52 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+* reset_to_98 - Updates the value it points to 98
+* @n: pointer to n
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+{
+*n = 98;
+}
+int _putchar(char c);
+void reset_to_98(int *n);
+}
