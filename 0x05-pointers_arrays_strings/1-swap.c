360789a17042805da8c6c01adb1817253079a4cd
k-ses <kelvinwachira02@gmail.com>
2022-06-14 03:40:44 -0700

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps value of twio integers
+ * @a: first ineger to swap
+ * @b: second integer to swap
+ * Return: 0
+*/
+void swap_int(int *a, int *b);
+{
+a = a + b;
+b = a - b;
+a = a - b;
+}
