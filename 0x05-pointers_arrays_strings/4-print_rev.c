84997579197d93fe4d91dc79e105a2712f5c2398
k-ses <kelvinwachira02@gmail.com>
2022-06-14 07:32:02 -0700

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_rev - print_rev block
+* @s: pointer type string
+* Description: prints a string in reverse follwed by a new line
+*/
+void print_rev(char *s)
+{
+int i;
+/* loops through the string an outputs its length*/
+for (i = 0; s[i] != '\0'; i++)
+{
+} 
+/*Reversing the string*/
+for (i--; i >= 0; i--)
+_putchar(s[i]);
+_putchar('\n');
+}
