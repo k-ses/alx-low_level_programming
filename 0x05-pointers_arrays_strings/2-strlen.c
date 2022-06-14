1a9f2aa8bb02d9d66c8e0190f5d206f6981e28db
k-ses <kelvinwachira02@gmail.com>
2022-06-14 05:21:58 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strlen - returns length of string
+ * @s: character to return length of
+ * Return: 0
+*/
+int _strlen(char *s);
+{
+char s[] = "Alx-low_level_programming";
+printf ("length of string s = %zu \n", strlen (s));
+return 0;
