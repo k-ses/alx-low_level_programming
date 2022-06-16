208a529b90fc9185834c7a4cc68aec2ba8588a99
k-ses <kelvinwachira02@gmail.com>
2022-06-16 06:17:29 -0700

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/**reverse_array - check the code for Holberton School students.
-* @a: an array of integers
-* @n: the number of elements to swap
-* Return: nothing.
+/**
+*reverse_array - check the code for Holberton School students.
+*@a: an array of integers
+*@n: the number of elements to swap
+*Return: nothing.
 */
 void reverse_array(int *a, int n)
 {
