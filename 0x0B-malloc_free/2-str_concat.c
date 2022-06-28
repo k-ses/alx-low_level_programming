4be0bee378a2a2c9d626dfe182eb1712aaa6af82
k-ses <kelvinwachira02@gmail.com>
2022-06-28 02:28:04 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,16 +14,16 @@ if (s1 == NULL)
 i = 0;
 else
 {
-for (i = 0; s1[i]; i++);
+for (i = 0; s1[i]; i++)
 }
 if (s2 == NULL)
 j = 0;
 else
 {
-for (j = 0; s2[j]; j++);
+for (j = 0; s2[j]; j++)
 }
 k = i + j + 1;
-s = malloc(k * sizeof(char));
+s = malloc(k *sizeof(char));
 if (s == NULL)
 return (NULL);
 for (l = 0; l < i; l++)
