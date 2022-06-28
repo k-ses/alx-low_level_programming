978c1aad648f9b8db7eb25d7623b165ef3eac7c8
k-ses <kelvinwachira02@gmail.com>
2022-06-28 02:37:36 -0700

    The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,9 +11,10 @@ unsigned int i, j;
 char *s;
 if (str == NULL)
 return (NULL);
-for (i = 0; str[i]; i++);
+for (i = 0; str[i]; i++)
+;
 i++;
-s = malloc(i * sizeof(char));
+s = malloc(i *sizeof(char));
 if (s == NULL)
 {
 return (NULL);
